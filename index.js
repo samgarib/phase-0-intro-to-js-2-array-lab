5cf362ab60481d79d29a71b0a5233cbe8c4efd00
samantha debenedictis <samdebenedictis@aol.com>
2022-05-31 16:25:20 -0400

    commit tree b480daa8ea43f3accd982c4a70f88dfc897cbd15

--- index.js
+++ index.js
@@ -1,1 +1,61 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendCat(name){
+
+
+    cats.push(name)
+}
+
+function destructivelyPrependCat(name){
+
+
+    cats.unshift(name)
+}
+
+function destructivelyRemoveLastCat(){
+
+
+    cats.pop()
+}
+
+function destructivelyRemoveFirstCat(){
+
+
+    cats.shift()
+}
+
+function appendCat(name){
+
+
+    const newCat = [...cats]
+
+    newCat.push(name)
+    return newCat
+}
+
+function prependCat(name){
+
+
+    const newCat = [...cats]
+
+    newCat.unshift(name)
+    return newCat
+}
+
+function removeLastCat(){
+
+
+    const newCat = [...cats]
+
+    newCat.pop()
+    return newCat
+}
+
+function removeFirstCat(){
+
+
+    const newCat = [...cats]
+
+    newCat.shift()
+    return newCat
+}
